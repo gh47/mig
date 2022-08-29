d8198db9f8b55855953b41ce6e860ce7d636fe8b
gh47 <gihwan_shin@daum.net>
2022-08-29 16:35:07 +0900

    Transational

--- src/main/java/com/mig/watch/service/MigJobService.java
+++ src/main/java/com/mig/watch/service/MigJobService.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
@@ -42,12 +44,17 @@ public class MigJobService {
 
             for (int i = 0; i < totalCnt; i+= divOutCnt ) {
                 List<Map<String, Object>> getSelectList = migSelectDao.migSelectMain(migSqlTblVO, i, divOutCnt);
-                migInsertDao.migInsertMain(migSqlTblVO, getSelectList);
-                getSelectList.clear();
+                this.saveList(migSqlTblVO, getSelectList);
             }
         }
         log.info("getSaltTstReqBdMain ----------------------------------------------------------------------------------------------------------- END");
         return rtn;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    private void saveList(MigSqlTblVO migSqlTblVO, List<Map<String, Object>> getSelectList) {
+        migInsertDao.migInsertMain(migSqlTblVO, getSelectList);
+        getSelectList.clear();
     }
 
 }
